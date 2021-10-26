50ec1c11e1830c012ec295e21ef03929ee48f433
B. Agovic <berminagovic@web.de>
2021-10-26 18:05:12 +0200

    working on couting output. it counts 1 charachter too much.

--- ft_printf.c
+++ ft_printf.c
@@ -6,61 +6,55 @@
 /*   By: bagovic <bagovic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 09:20:46 by bagovic           #+#    #+#             */
-/*   Updated: 2021/10/25 17:57:56 by bagovic          ###   ########.fr       */
+/*   Updated: 2021/10/26 18:04:46 by bagovic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-//cspdiuxX%
+static int	ft_numlen(int num)
+{
+	int	len;
 
-// static char	ft_check_wildcard(const char *format)
-// {
-// 	char	*wildcards;
-// 	int		format_i;
-// 	int		wc_i;
+	len = 0;
+	while (num != 0 || len == 0)
+	{
+		num = num / 10;
+		len++;
+	}
+	return (len);
+}
 
-// 	wildcards = ft_get_wildcards();
-// 	format_i = 0;
-// 	while (format[format_i] != '\0')
-// 	{
-// 		if (format[format_i] == '%')
-// 		{
-// 			format_i++;
-// 			wc_i = 0;
-// 			while (wc_i < 9)
-// 			{
-// 				if (format[format_i] == wildcards[wc_i])
-// 					return (1);
-// 				wc_i++;
-// 			}
-// 		}
-// 		format_i++;
-// 	}
-// 	return (0);
-// }
 //cspdiuxX%
-static void	ft_printwc(va_list valist, char wc)
+static int	ft_printwc(va_list valist, char wc)
 {
 	char	*p;
 
 	if ((int) wc == '%')
 	{
 		write (1, &wc, 1);
-		return ;
+		return (1);
 	}
 	p = va_arg(valist, char *);
 	if ((int) wc == 'c')
+	{
 		ft_putchar_fd((char) p, 1);
+		return (1);
+	}
 	else if (wc == 's')
+	{
 		ft_putstr_fd(p, 1);
-	// else if (*wc == 'p')
-	// {}
+		return (ft_strlen(p));
+	}
 	else if ((int) wc == 'd')
+	{
 		ft_putnbr_fd((int) p, 1);
+		return (ft_numlen((int) p));
+	}
+	return (0);
 }
 
-static void	ft_flwc(va_list	valist, char *flwc)
+static int	ft_flwc(va_list	valist, char *flwc)
 {
 	int	i;
 	int	y;
@@ -72,23 +66,23 @@ static void	ft_flwc(va_list	valist, char *flwc)
 		while (y < 9)
 		{
 			if (flwc[i] == g_wildcards[y])
-			{
-				ft_printwc(valist, flwc[i]);
-				return ;
-			}
+				return (ft_printwc(valist, flwc[i]));
 			y++;
 		}
 		i++;
 	}
+	return (0);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	valist;
 	char	*flwc;
+	int		output_count;
 	int		i;
 
 	ft_initialize_data();
+	output_count = 0;
 	i = 0;
 	va_start(valist, format);
 	while (format[i] != '\0')
@@ -96,21 +90,25 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			flwc = ft_substr(format, i + 1, ft_strlen(format) - (i + 1));
-			ft_flwc(valist, flwc);
+			output_count += ft_flwc(valist, flwc);
 			i++;
 		}
 		else
+		{
 			write(1, &format[i], 1);
+			output_count++;
+		}
 		i++;
 	}
 	va_end(valist);
-	return (0);
+	return (output_count);
 }
 
 int	main(void)
 {
-	ft_printf("Hello %% %s", "World!");
-	// ft_printf("Hello%d%%%c %s\n", 13134, ',', "World!");
+	// ft_printf("Hello %% %s", "World!");
+	ft_printf("%d", ft_printf("Hello%d%%%c %s\n", 13134, ',', "World!"));
+	//Hello13134%, World!
 	// // printf("Hello %#s", "world!");
 	return (0);
 }
